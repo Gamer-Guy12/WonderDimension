60dfff659d1bcfabcb80afe2aea3152d40b4229d
Varunan Sanjeepan <varunan.sanjeepan@outlook.com>
2023-12-26 09:50:43 -0800

    quick thing

--- src/main/java/net/da_fence85/wonderdimension/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/da_fence85/wonderdimension/datagen/loot/ModBlockLootTables.java
@@ -21,7 +21,7 @@ import java.util.Set;
 
 public class ModBlockLootTables extends BlockLootSubProvider {
 
-    protected ModBlockLootTables() {
+    public ModBlockLootTables() {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags());
     }
 
